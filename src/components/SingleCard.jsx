f0d06aca5908efcf65b275c2343e41ab305249a7
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-11-17 10:33:53 +0900

    카드 뒷면 클릭 시 콘솔 표시

--- src/components/SingleCard.jsx
+++ src/components/SingleCard.jsx
@@ -1,12 +1,20 @@
 import "./SingleCard.css";
 import React from "react";
 
-const SingleCard = (props) => {
+const SingleCard = ({ card }) => {
+  function handleClick() {
+    console.log(card);
+  }
   return (
-    <div className="card" key={props.id}>
+    <div className="card">
       <div>
-        <img className="front" src={props.card.src} alt="card front"></img>
-        <img className="back" src="/img/cover.png" alt="card back"></img>
+        <img className="front" src={card.src} alt="card front"></img>
+        <img
+          onClick={handleClick}
+          className="back"
+          src="/img/cover.png"
+          alt="card back"
+        ></img>
       </div>
     </div>
   );
