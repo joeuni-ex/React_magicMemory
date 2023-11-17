e5b76ff1c5e5e30f81646e541e5d95f4a03d419b
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-11-17 10:15:17 +0900

    카드를 화면표시 및 CSS적용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+//img폴더안에 이미지 사용
+//src의 key 값으로 이미지의 주소 값
+const cardImages = [
+  { src: "/img/helmet-1.png" },
+  { src: "/img/potion-1.png" },
+  { src: "/img/ring-1.png" },
+  { src: "/img/scroll-1.png" },
+  { src: "/img/shield-1.png" },
+  { src: "/img/sword-1.png" },
+];
 
 function App() {
+  // 카드와 턴수 useState 만들기
+  // useState 사용
+  const [cards, setCards] = useState([]); //카드는 배열로 시작함
+  const [turns, setTurns] = useState(0);
+
+  const shuffleCards = () => {
+    //카드 섞기
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards); //섞은 카드를 저장
+    setTurns(0); // 턴 수를 0으로
+  };
+  console.log(cards, turns);
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
+      <div className="card-grid">
+        {cards.map((card) => (
+          <div className="card" key={card.id}>
+            <div>
+              <img className="front" src={card.src} alt="card front"></img>
+              <img className="back" src="/img/cover.png" alt="card back"></img>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
-export default App
+export default App;
